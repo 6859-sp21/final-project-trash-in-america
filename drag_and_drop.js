847036ce8f78a273569cb2562a0b19006194f616
kalynsensei <kalyn.bowen@gmail.com>
2021-04-29 02:50:46 -0700

    more drag-and-drop page functionalities implemented

--- drag_and_drop.js
+++ drag_and_drop.js
@@ -70,10 +70,8 @@ function drop(e) {
   const targetClassList = e.target.classList;
   if (targetClassList.contains("trash-category")) {
     // add it to the drop target
-    console.log("yayay");
     e.target.appendChild(draggable);
   } else {
-    console.log("here");
     unsorted_region.appendChild(draggable);
   }
 
@@ -82,3 +80,58 @@ function drop(e) {
   //draggable.style.left = "0px";
   //draggable.style.top = "0px";
 }
+
+const answers = {
+  "bread-slice": "compostable",
+  "hamburger": "compostable",
+  "drumstick-bite": "compostable",
+  "car":"trashable",
+  "bus": "trashable", 
+  "ambulance":"trashable",
+  "kiwi-bird": "recyclable",
+  "cat": "recyclable",
+  "horse": "recyclable"
+};
+
+document.getElementById("trash-test-submit").onclick = function checkSorting() {
+  let score = 0;
+  let wrongItemIds = [];
+  for (let key in answers) {
+    const trashItem = document.getElementById(key);
+    const parent = trashItem.parentElement;
+    if (parent.id === answers[key]) {
+      score += 1;
+    } else {
+      wrongItemIds.push(key);
+    }
+  }
+  const answersDiv = document.getElementById("answers");
+  answersDiv.innerHTML = "";
+  let p1 = document.createElement('p');
+  p1.textContent = `You got ${score} out of ${Object.keys(answers).length} items correct`;
+  let p2 = document.createElement('p');
+  if (score === 9) {
+    p2.textContent = "Wow, you sorted everything correctly!";
+  } else {
+    p2.textContent = "Let's learn how to sort the items you sorted incorrectly";
+  }
+  answersDiv.appendChild(p1);
+  answersDiv.appendChild(p2);
+  for (let i = 0; i < wrongItemIds.length; i++) {
+    let span = document.createElement('span');
+    let img = document.createElement('img');
+    img.src = "images/" + wrongItemIds[i] + ".svg";
+    img.classList.add("wrong-trash-item");
+    span.appendChild(img);
+    span.innerHTML += "is&nbsp;";
+    let font = document.createElement('font');
+    font.innerText = answers[wrongItemIds[i]];
+    font.classList.add(answers[wrongItemIds[i]]);
+    span.appendChild(font);
+    span.classList.add("wrongItem");
+    answersDiv.appendChild(span);
+  }
+  answersDiv.scrollIntoView({behavior: "smooth"});
+};
+
+// look at this later https://www.sfweekly.com/news/which-bin-does-it-go-in/
